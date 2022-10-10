229cce3bf3da3b695381a6d5e03d4bd633b15577
Jono Tuke <jonotuke@gmail.com>
2022-10-10 16:25:00 +1030

    Added proportions for DF

---
+++ R/exam_convert_prop.R
@@ -0,0 +1,26 @@
+#' exam_convert_prop
+#'
+#' given a dataframe, some columns and a total row, converts
+#' to proportions
+#'
+#' @param df a dataframe with totals in one row
+#' @param cols tidyselection of columns
+#' @param row number of row with totals
+#'
+#' @return dataframe with proportions
+#' @export
+#'
+#' @examples
+exam_convert_prop <- function(df, cols, row){
+  df |>
+    mutate(
+      across({{cols}}, exam_get_prop, row)
+    )
+}
+# pacman::p_load(tidyverse, targets)
+# df <- tibble(
+#   A1 = c(10, 1, 2),
+#   A2 = c(10, 2, 1)
+# )
+# df |> exam_convert_prop(A1, 1)
+# df |> exam_convert_prop(A1:A2, 1)
--- tests/testthat/test-exam_convert_prop.R
+++ tests/testthat/test-exam_convert_prop.R
@@ -1,3 +1,13 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  pacman::p_load(tidyverse)
+  df <- tibble(
+    A1 = c(10, 1, 2),
+    A2 = c(10, 2, 1)
+  )
+  df1 <- df |> exam_convert_prop(A1, 1)
+  df2 <- df |> exam_convert_prop(A1:A2, 1)
+
+  expect_equal(df1$A1, c(1, 0.1, 0.2))
+  expect_equal(df1$A2, c(10, 2, 1))
+  expect_equal(df2$A2, c(1, 0.2, 0.1))
 })
